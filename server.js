b9afacf0ca8f7b07c51b3e3265d7fc9de4f4fc13
FKMLJF <vincze.jozsef.develop@gmail.com>
2021-09-19 16:14:38 +0200

    include body parser
    #4

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
-
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
